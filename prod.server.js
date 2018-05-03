53473ca6cb37949a1b0af5b3129c9ad54a5ef65b
Hale Deng <haledeng88@gmail.com>
2018-05-03 16:42:56 +0800

    update

--- prod.server.js
+++ prod.server.js
@@ -20,21 +20,21 @@ const ratings = appData.ratings
 
 let apiRoutes = express.Router()
 
-apiRoutes.get('seller', function (req, res) {
+apiRoutes.get('/seller', function (req, res) {
   res.json({
     errno: 0,
     data: seller
   })
 })
 
-apiRoutes.get('goods', function (req, res) {
+apiRoutes.get('/goods', function (req, res) {
   res.json({
     errno: 0,
     data: goods
   })
 })
 
-apiRoutes.get('ratings', function (req, res) {
+apiRoutes.get('/ratings', function (req, res) {
   res.json({
     errno: 0,
     data: ratings

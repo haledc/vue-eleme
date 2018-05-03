c6434f094c9ad1e33faa6c4a7dcafe0663f338b3
Hale Deng <haledeng88@gmail.com>
2018-05-03 11:15:06 +0800

    update comment

--- src/common/js/store.js
+++ src/common/js/store.js
@@ -5,17 +5,20 @@
  * @param value
  */
 export function saveToLocal(id, key, value) {
+  // 手动设置localStorage的key
   let seller = window.localStorage.__seller__
   if (!seller) {
     seller = {}
     seller[id] = {}
   } else {
+    // 取出的缓存为JSON格式，需要转换为js格式
     seller = JSON.parse(seller)
     if (!seller[id]) {
       seller[id] = {}
     }
   }
   seller[id][key] = value
+  // 缓存对象为JSON格式
   window.localStorage.__seller__ = JSON.stringify(seller)
 }
 
@@ -23,8 +26,8 @@ export function saveToLocal(id, key, value) {
  * 读取缓存数据
  * @param id
  * @param key
- * @param def
- * @return {*}
+ * @param def 实际值
+ * @return {*} Boolean
  */
 export function loadFromLocal(id, key, def) {
   let seller = window.localStorage.__seller__
@@ -36,5 +39,6 @@ export function loadFromLocal(id, key, def) {
     return def
   }
   let ret = seller[key]
+  // 返回缓存的值，没有就返回实际的值
   return ret || def
 }
